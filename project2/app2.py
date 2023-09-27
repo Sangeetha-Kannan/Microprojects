7c15a6ffeb743a9bfdd5cbce3244a210889d3f9e
Sangeetha Kannan <118890166+Sangeetha-Kannan@users.noreply.github.com>
2023-09-27 15:47:47 +0530

    projecy2

---
+++ project2/app2.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 15:42:05 2023
+
+@author: sangeetha
+"""
+
+import streamlit as st
+import os
+import pandas as pd
+
+# Defining folder paths
+glb_root = r"C:\Users\sangeetha\OneDrive\DSM_projects\18_data_apps\Microprojects\project2"
+glb_01_input = os.path.join(glb_root, "01_input")
+glb_02_master = os.path.join(glb_root, "02_master")
+glb_03_output = os.path.join(glb_root, "03_output")
+glb_04_temp_input = os.path.join(glb_root, "04_temp_input")
+glb_masterfile = os.path.join(glb_02_master, "dummy_master.csv")
+
+# Create working directories if not existing
+for folder in [glb_root, glb_01_input, glb_03_output, glb_04_temp_input]:
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+
+
+# Streamlit app
+def main():
+    st.title("File Upload and Processing App")
+
+    # File upload section
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+    if uploaded_file is not None:
+        # Check if the file is uploaded
+        st.success("File uploaded successfully")
+
+        # Save the uploaded file to the input directory
+        file_path = os.path.join(glb_01_input, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+
+        # Process the client_raw data as needed
+
+        # You can display the processed data or any other information to the user here
+        st.dataframe(client_raw)  # Display the DataFrame
+
+if __name__ == "__main__":
+    main()
